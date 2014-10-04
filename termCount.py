3a19e5b8c7cf0400d47d7b2e8dd42aac2989c805
Rajarshi Guha <rajarshi.guha@gmail.com>
2014-10-04 13:38:29 -0400

    Started on the introduction

--- termCount.py
+++ termCount.py
@@ -2,7 +2,7 @@ import urllib
 import xml.etree.ElementTree as ET
 
 u = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?term=%s&mindate=%d/01/01&maxdate=%d/12/31"
-term = "QSAR"
+term = "QSAR or QSPR"
 startYear = 1980
 endYear = 2013
 for year in range(startYear, endYear+1):
