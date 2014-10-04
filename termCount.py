5632983ea4a4e2d6c8d48c7390f0c4ca72e626e7
Rajarshi Guha <rajarshi.guha@gmail.com>
2014-10-04 11:41:01 -0400

    initial repo for paper

---
+++ termCount.py
@@ -0,0 +1,13 @@
+import urllib
+import xml.etree.ElementTree as ET
+
+u = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?term=%s&mindate=%d/01/01&maxdate=%d/12/31"
+term = "QSAR"
+startYear = 1980
+endYear = 2013
+for year in range(startYear, endYear+1):
+    url = u % (term.replace(" ", "+"), year, year)
+    page = urllib.urlopen(url).read()
+    doc = ET.XML(page)
+    count = doc.find("Count").text
+    print year, count
